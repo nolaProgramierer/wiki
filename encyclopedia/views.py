bfdf0bef7a4cde74507c05e5b27015cfe5816999
Glenn Langdon <glennlangdon@gmail.com>
2020-06-30 22:29:33 -0600

    Search complete

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -4,12 +4,34 @@ from django.http import HttpResponse
 
 from . import util
 
-
+# GET request in form returns query result
+# or the GET request returns list of all encyclopedia entries
 def index(request):
+    query = request.GET.get("q")
+
+    if util.get_entry(query):
+        return render(request, "encyclopedia/entry.html", {
+            "entries": util.get_entry(query)
+        })
+    # Check GET query in encycclopedia entries
+    elif util.get_entry(query) is not query:
+        entrylist = []
+        for entry in util.list_entries():
+            if query in entry:
+                entrylist.append(entry)          
+        context = {
+            "entries": util.list_entries(),
+            "entrylist": entrylist,
+            "query": query,
+            "message": f"'{query}' does not match any entry."
+        }
+        return render(request, "encyclopedia/search.html", context)
+   
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(), "messsge":"hello"
     })
 
+# Returns emtry and definition of title
 def entry(request, title):
     if util.get_entry(title) is None:
         return render(request, "encyclopedia/error.html", {
